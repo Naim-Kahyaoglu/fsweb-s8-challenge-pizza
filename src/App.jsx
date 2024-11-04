2b24f88a890d9b05d6a8596f7414ecaa09f74950
Naim-Kahyaoglu <naimtrainingweb753@gmail.com>
2024-11-04 12:12:54 +0300

    it1-test kurulumu

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import OrderForm from './OrderForm';
-import OrderSuccess from './OrderSuccess';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import OrderForm from "./components/OrderForm";
+import OrderSuccess from "./components/OrderSuccess";
 
 function App() {
-    return (
-        <Router>
-            <Routes>
-                <Route path="/" element={<Home />} /> {/* Anasayfa bileşeni */}
-                <Route path="/order" element={<OrderForm />} />
-                <Route path="/success" element={<OrderSuccess />} />
-            </Routes>
-        </Router>
-    );
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} /> {/* Anasayfa bileşeni */}
+        <Route path="/order" element={<OrderForm />} />
+        <Route path="/order-success" element={<OrderSuccess />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
