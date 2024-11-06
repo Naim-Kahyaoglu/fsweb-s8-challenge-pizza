a198d3bc8dc5c66c9f80868299d9eb2779b92933
Naim-Kahyaoglu <naimtrainingweb753@gmail.com>
2024-11-06 15:54:56 +0300

    son form düzenlemeleri yapıldı.validasyon öncesindeyiz

--- src/components/OrderForm.jsx
+++ src/components/OrderForm.jsx
@@ -152,15 +152,16 @@ const OrderForm = () => {
       </div>
 
       <div className="pizza-info">
-        <h2>Position Absolute Acı Pizza</h2>
-        <div className="price-rating">
-          <span className="price">85.50₺</span>
-          <span className="rating">4.9 (200)</span>
-        </div>
-        <p className="description">
-          Frontend Dev olarak hala position:absolute kullanıyorsan bu çok acı pizza tam sana göre. 
-        </p>
-      </div>
+  <h2>Position Absolute Acı Pizza</h2>
+  <div className="price-rating">
+    <span className="price">85.50₺</span>
+    <span className="rating">4.9 (200)</span>
+  </div>
+  <p className="description">
+    Frontend Dev olarak hala position:absolute kullanıyorsan bu çok acı pizza tam sana göre. Pizza, domates, peynir ve genellikle çeşitli diğer malzemelerle kaplanmış, daha sonra geleneksel olarak odun ateşinde bir fırında yüksek sıcaklıkta pişirilen, genellikle yuvarlak, düzleştirilmiş mayalı buğday bazlı hamurdan oluşan İtalyan kökenli lezzetli bir yemektir. Küçük bir pizzaya bazen pizzetta denir.
+  </p>
+</div>
+
 
       <form onSubmit={handleSubmit}>
         {/* Boyut ve Hamur Seçimi */}
